f6f689100a94e6d0626b43cbb82f312d71e70b7e
Kaleb Mesfin <kalemesfin12@gmail.com>
2023-04-24 17:39:17 +0300

    fix(web): made changes to origin photos and components

--- components/sources/SourceItem.tsx
+++ components/sources/SourceItem.tsx
@@ -1,4 +1,5 @@
 import React, { Dispatch, SetStateAction } from "react";
+import Image from "next/image";
 import { FaChevronRight } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { Region } from "../../pages/our-coffee/origins";
@@ -11,18 +12,18 @@ type Props = {
 const SourceItem = ({ region, setSelectedId }: Props) => {
   return (
     <motion.div
-      className="last:w-full last:flex last:justify-center w-full h-96 p-2 rounded-2xl shadow-md bg-white cursor-pointer"
+      className="w-full md:w-[45%] h-96 p-2 rounded-2xl shadow-md bg-white cursor-pointer"
       layoutId={region.id.toString()}
       onClick={() => setSelectedId(region.id)}
     >
-      <motion.div
-        style={{ backgroundImage: `url('${region.src}')` }}
-        className="relative h-full bg-no-repeat bg-contain w-full bg-center"
-      >
-        <motion.div className="absolute bottom-8 left-8 flex flex-col justify-end">
+      <motion.div className="flex flex-col w-full h-full gap-y-2 px-8 md:px-6 xl:px-8">
+        <div className="w-full h-4/6">
+          <img src={region.src} alt="" className="w-full h-full" />
+        </div>
+        <motion.div className="flex flex-col justify-end">
           <motion.p>{region.description}</motion.p>
           <motion.div className="flex items-center gap-x-2">
-            <motion.p className="font-DM_Serif text-5xl text-start text-secondary">
+            <motion.p className="font-DM_Serif text-5xl md:text-4xl xl:text-5xl text-start text-secondary">
               {region.name}
             </motion.p>
             <motion.div>
